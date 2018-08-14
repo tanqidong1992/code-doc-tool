23eaf0b3cd87f483acf6e5868d785508be21d6b5
tqd <903843602@qq.com>
2018-08-14 13:54:13 +0800

    完善JS代码生成

--- codegen/src/main/java/org/codegen/JSCodeGenerator.java
+++ codegen/src/main/java/org/codegen/JSCodeGenerator.java
@@ -26,10 +26,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.xmlbeans.impl.xb.xmlconfig.Extensionconfig.Interface;
 import org.beetl.core.Configuration;
 import org.beetl.core.GroupTemplate;
 import org.beetl.core.Template;
+import org.beetl.core.resource.ClasspathResourceLoader;
 import org.beetl.core.resource.FileResourceLoader;
 import org.beetl.core.resource.StringTemplateResourceLoader;
 import org.springframework.stereotype.Controller;
@@ -37,14 +37,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.hngd.doc.core.InterfaceInfo.RequestParameterInfo;
+import com.hngd.doc.core.MethodInfo;
 import com.hngd.doc.core.ModuleInfo;
 import com.hngd.doc.core.gen.SwaggerDocGenerator;
+import com.hngd.doc.core.parse.CommonClassCommentParser;
 import com.hngd.doc.core.parse.ControllerClassCommentParser;
 import com.hngd.doc.core.parse.ModuleParser;
 import com.hngd.doc.core.util.TypeUtils;
 import com.hngd.doc.core.web.ModuleParserTest;
-import com.hngd.util.GsonUtils;
-import com.hngd.web.controller.*;
+ 
  
 
 /**
@@ -52,7 +54,6 @@ import com.hngd.web.controller.*;
  */
 public class JSCodeGenerator
 {
-    public static List<Class<?>>            cls             = Arrays.asList(SystemController.class);
     public static final Map<String, String> SPRING2RETROFIT = new HashMap<String, String>();
     static
     {
@@ -62,47 +63,71 @@ public class JSCodeGenerator
     public static final String[] CONTROLLER_CLASS_SRC_DIC = {
 	"F:\\HNOE_TQD_JAVA\\JavaCode\\HNVMNS6000\\web\\src\\main\\java\\com\\hngd\\web\\controller" };
 
-    static Map<String, List<ModuleInfo>> map = new HashMap<>();
- 
-/*    public static void useFreemake(){
-    	File templateFile = new File("templates/jscode.txt");
-         Configuration cfg = new Configuration(Configuration.VERSION_2_3_23);
-         File dir = new File("temp");
-         try {
-			cfg.setDirectoryForTemplateLoading(dir);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-         cfg.setDefaultEncoding("UTF-8");
-         cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-         Template t=null;
-		try {
-			t = new Template("jsCOdes", new FileReader(templateFile), cfg);
-		} catch (IOException e1) {
-			 
-			e1.printStackTrace();
-		}
-         String filePath="F:\\HNOE_TQD_DOC\\HN\\HNFrontToEnd\\common\\js\\client.js";
-          
-		try (FileOutputStream fos= FileUtils.openOutputStream(new File(filePath));
-				Writer out = new OutputStreamWriter(fos);){
-			 t.process(map, out);
-			 out.flush();
-		} catch (IOException | TemplateException e1) {
-			 e1.printStackTrace();
-		}
- 
-    }*/
+    static Map<String, Object> map = new HashMap<>();
+    
     public static void main(String[] args) throws IOException
     {
-        useBeetl();
+    	String serviceUrl="/auth";
+        String filePath="./src/main/java/org/codegen/HttpTestCodeGenerator.java";
+        filePath="E:\\Code\\STSCode\\hnvmns-auth\\src\\main\\java\\com\\hngd\\web\\controller\\MenuController.java";
+        
+		//useBeetl();
+    	String str=generate(filePath,serviceUrl);
+    	System.out.println(str);
     }
+    
+    public static String generate(String javaFilePath,String serviceUrl) throws IOException{
+    	
+    	File f=new File(javaFilePath);
+    	
+    	List<ModuleInfo> modules=ModuleParser.parse(f);
+    	if(modules==null){
+    		return null;
+    	}
+    	CommonClassCommentParser.parse(f);
+    	attachCommentInfo(modules);
+    	map.put("serviceUrl", serviceUrl);
+    	map.put("modules", modules);
+    	ClasspathResourceLoader resourceLoader = new ClasspathResourceLoader("/beetl", "utf-8");
+		Configuration cfg = Configuration.defaultConfiguration();
+		GroupTemplate gt = new GroupTemplate(resourceLoader, cfg);
+		Template t = gt.getTemplate("jscode.txt");
+		t.binding(map);
+		String str = t.render();
+		return str;
+    	
+    }
+    
+	private static void attachCommentInfo(List<ModuleInfo> modules) {
+		for(ModuleInfo mi:modules){
+			mi.interfaceInfos.forEach(ii->{
+				
+				String key=mi.className+"#"+ii.methodName;
+				MethodInfo mm=CommonClassCommentParser.methodComments.get(key);
+				if(mm!=null){
+					ii.comment=mm.comment;
+					for(int i=0;i<ii.parameterInfos.size();i++){
+						RequestParameterInfo pi=ii.parameterInfos.get(i);
+						if(i<mm.parameters.size()){
+							pi.comment=mm.parameters.get(i).comment;
+						}
+					}
+				}
+				
+			});
+			
+			
+		}
+		
+	}
+
 	private static void useBeetl() throws IOException {
 		File f=new File("E:\\Code\\STSCode\\hnvmns-auth\\src\\main\\java\\com\\hngd\\web\\controller\\MenuController.java");
     	List<ModuleInfo> modules=ModuleParser.parse(f);
-    	//System.out.println(GsonUtils.toJsonString(modules));
+    	
+		//System.out.println(GsonUtils.toJsonString(modules));
     	map.put("modules", modules);
-		FileResourceLoader resourceLoader = new FileResourceLoader("./beetl", "utf-8");
+    	ClasspathResourceLoader resourceLoader = new ClasspathResourceLoader("/beetl", "utf-8");
 		Configuration cfg = Configuration.defaultConfiguration();
 		GroupTemplate gt = new GroupTemplate(resourceLoader, cfg);
 		Template t = gt.getTemplate("jscode.txt");
