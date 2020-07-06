da9762eb7e73aa3376406de9718ba9a820317ec0
QidongTan <QidongTan@qq.com>
2020-07-06 11:44:03 +0800

    合并开发与生产环境配置文件,修正上传接口文档上传大小限制到100mb

--- swagger-ui/src/main/java/com/hngd/doc/constants/Constants.java
+++ swagger-ui/src/main/java/com/hngd/doc/constants/Constants.java
@@ -2,7 +2,7 @@ package com.hngd.doc.constants;
 
 public class Constants {
     /**
-     * 上传文件大小限制10M
+     * 上传文件大小限制100M
      */
-	public static final int MAX_UPLOAD_FILE_SIZE=10*1024*1024;
+	public static final int MAX_UPLOAD_FILE_SIZE=100*1024*1024;
 }
---
+++ swagger-ui/src/test/java/com/hngd/code/OpenApiGeneratorTest.java
@@ -0,0 +1,19 @@
+package com.hngd.code;
+
+public class OpenApiGeneratorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		org.openapitools.codegen.OpenAPIGenerator.main(new String[]{"generate",
+				"-i",
+				"W:\\study\\vsts-rest-api-specs\\specification\\git\\tfs-4.1\\git-onprem.json",
+				"-g",
+				"java",
+				"-o",
+				"./test-out"});
+				*/
+	//a=	com.fasterxml.jackson.core.JsonFactory.DEFAULT_ROOT_VALUE_SEPARATOR;
+	}
+
+}
